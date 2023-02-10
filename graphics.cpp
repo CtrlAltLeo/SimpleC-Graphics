7f91b31296a3f81c7e7de6a671d174da0dc83955
Leo Devick <leodevick@gmail.com>
2023-02-10 13:11:45 -0600

    display class

--- graphics.cpp
+++ graphics.cpp
@@ -17,141 +17,151 @@
 
 using namespace std;
 
-const char null = '~';
 
 const int SCREEN_X = 70;
 const int SCREEN_Y = 30;
 
-char screen[SCREEN_Y][SCREEN_X] = {};
 
 
-void _draw(){
-	int x,y;
-	for (y = 0; y < SCREEN_Y; y++){
-
-		string h_line = "";
-
-		for (x = 0; x < SCREEN_X; x++){
-			
-				h_line = h_line + screen[y][x];		
-
-		}
-
-		cout << h_line << endl;
-	}
-}
-
-void set_char(int x, int y, char c){
+class display_screen{
 	
-	if (c == null){
-		return;
-	}
 
-
-	screen[y][x] = c;
-
-}
-
-void cls(){
-	cout << "\033[2J\033[1;1H";
-	
-	//Maybe it clears the draw array too?
-
-
-}
-
-void rect_fill(int x1, int y1, int x2, int y2, char fill){
-	
-	int x,y;
-	for (y = y1; y < y2; y++){
-		for (x = x1; x < x2; x++){
-			
-			set_char(x,y,fill);
-
-		}
-	}
-
-}
-
-
-//this doesn't work :]
-void rect(int x1, int y1, int x2, int y2, char c, int w = 2){
-
-	int x,y;
-
-	for (y = y1; y < y2; y++){
-		for (x = x1; x < x2; x++){
-			
-
-
-
-		}
-	}
-
-}
-
-//Neither does this. Angle is wrong for some reason.
-void line(int x1, int y1, int x2, int y2, char c){
-
-	float m = atan((y2-y1)/(x2-x1));
+	private:	
 		
-	int x,y;
-	for (x = x1; x < x2; x++){
-		
-		set_char( floor(x*cos(m)),floor( y * sin(m)), c);
-
-	}	
-
-
-
-}
-
-
-void print(int x, int y, string text){
+		const char null = '~';
+		char screen[SCREEN_Y][SCREEN_X] = {};
 	
-	int i;
-	for (i = 0; i < text.length(); i++){
-	
-		set_char(x+i,y,text[i]);
+	public:
 
-	}
+		void draw(){
+				int x,y;
+			for (y = 0; y < SCREEN_Y; y++){
+
+				string h_line = "";
+
+				for (x = 0; x < SCREEN_X; x++){
+					
+						h_line = h_line + screen[y][x];		
+
+				}
+
+				cout << h_line << endl;
+			}
+		}
+
+		void set_char(int x, int y, char c){
+			
+			if (c == null){
+				return;
+			}
 
 
-}
+			screen[y][x] = c;
+
+		}
+
+		void cls(){
+			cout << "\033[2J\033[1;1H";
+			
+			//Maybe it clears the draw array too?
 
 
-//prints in a box, i.e. dialouge or inventory
-void bounds_print(){
+		}
 
-}
+		void rect_fill(int x1, int y1, int x2, int y2, char fill){
+			
+			int x,y;
+			for (y = y1; y < y2; y++){
+				for (x = x1; x < x2; x++){
+					
+					set_char(x,y,fill);
 
-void ascii_image(int x, int y, string path){
-
-	ifstream art;
-	art.open(path);
-	
-	string line;
-	
-	int Y = 0;
-
-	while(getline(art, line)){
-
-		for (int i = 0; i < line.length(); i++){
-			if (line[i] == ' '){
-				line[i] = null;
+				}
 			}
 
 		}
 
 
-		//displays the line	
-		print(x,y+Y,line);
-		Y += 1;
-	}
+		//this doesn't work :]
+		void rect(int x1, int y1, int x2, int y2, char c, int w = 2){
+
+			int x,y;
+
+			for (y = y1; y < y2; y++){
+				for (x = x1; x < x2; x++){
+					
 
 
-}
 
+				}
+			}
+
+		}
+
+		//Neither does this. Angle is wrong for some reason.
+		void line(int x1, int y1, int x2, int y2, char c){
+
+			float m = atan((y2-y1)/(x2-x1));
+				
+			int x,y;
+			for (x = x1; x < x2; x++){
+				
+				set_char( floor(x*cos(m)),floor( y * sin(m)), c);
+
+			}	
+
+
+
+		}
+
+
+		void print(int x, int y, string text){
+			
+			int i;
+			for (i = 0; i < text.length(); i++){
+			
+				set_char(x+i,y,text[i]);
+
+			}
+
+
+		}
+
+
+		//prints in a box, i.e. dialouge or inventory
+		void bounds_print(){
+
+		}
+
+		void ascii_image(int x, int y, string path){
+
+			ifstream art;
+			art.open(path);
+			
+			string line;
+			
+			int Y = 0;
+
+			while(getline(art, line)){
+
+				for (int i = 0; i < line.length(); i++){
+					if (line[i] == ' '){
+						line[i] = null;
+					}
+
+				}
+
+
+				//displays the line	
+				print(x,y+Y,line);
+				Y += 1;
+			}
+
+
+		}
+		
+		
+};
 
 int main(){
 	
@@ -160,25 +170,24 @@ int main(){
 	x = 0;
 	y = 0;
 
-	int ly = 10;
+	display_screen display;	
 
 	while (true){
 
-		cls();
-	
-		rect_fill(0,0,SCREEN_X,SCREEN_Y,'`');
-		
-		ascii_image(5,-2,"test");
+		display.cls();
 
-		x += 1;
-		y += 1;
+		display.rect_fill(0,0, SCREEN_X, SCREEN_Y, '-');
 
-		rect_fill(2,SCREEN_Y/2, SCREEN_X-2,SCREEN_Y-2,'%');
+		display.ascii_image(1,1,"test");
 
-		rect_fill(3,SCREEN_Y/2+1, SCREEN_X-3,SCREEN_Y-3,'.');
+		display.rect_fill(2,SCREEN_Y/2, SCREEN_X - 2, SCREEN_Y-2, '&');
 
-		print(5, SCREEN_Y/2+3,"HEY! YOU'RE FINALLY AWAKE!");
-				_draw();	
+		display.rect_fill(3,SCREEN_Y/2+1, SCREEN_X - 3, SCREEN_Y-3, '.');
+
+
+		display.print(4,SCREEN_Y/2+2, "Hey you, you're finally awake. You were trying to cross the border..");
+
+		display.draw();
 			
 		sleep(1);
 
